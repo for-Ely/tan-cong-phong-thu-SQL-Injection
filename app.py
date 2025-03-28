87b4c469b9f1da11321b62720d55cf580ee13121
for-Ely <kaneki21174@gmail.com>
2025-03-28 19:43:18 +0700

    week 2
    add profile for user
    still have bug with mysql

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 from db import conn
 
 app = Flask(__name__)
+app.secret_key = 'this_is_a_secret_key'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,13 +15,51 @@ def login():
         query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
         cur.execute(query)
         user = cur.fetchone()
-
+        cur.close()
+        print(user)
         if user:
-            return f"Welcome {user[1]}!"
+            session['username'] = user[1]
+            return redirect('/profile')
         else:
             return "Invalid credentials!"
 
     return render_template('login.html')
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    # Nếu chưa đăng nhập, redirect đến trang đăng nhập
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+    # Nếu đã đăng nhập, hiển thị trang profile
+    username = session['username']
+    cur = conn.cursor()
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    cur.execute(query)
+    user = cur.fetchone()
+    cur.close()
+    
+    if request.method == 'POST':
+        # Lấy dữ liệu từ form
+        user_id = user[0]
+        email = request.form['email']
+        age = request.form['age']
+
+        # Cập nhật thông tin người dùng trong cơ sở dữ liệu
+        cur = conn.cursor()
+        query = f"UPDATE users SET email = {email}, age = {age} WHERE user_id = {user_id}"
+        cur.execute(query)
+        cur.close()
+    if not user:
+        return "User not found!"
+    else:
+        # Hiển thị thông tin người dùng
+        return render_template('profile.html',user_id=user[0], username=user[1], email=user[3], age=user[4])
+    
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
